837bd4731d13a0776981c14198387814529a2bea
sourabh pande <sourabhpande43@gmail.com>
2022-06-04 21:32:57 +0530

    commited

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bcrypt = require("bcrypt")
 const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 const path = require("path")
 const hbs = require("hbs");
 const cookie_parser = require("cookie-parser");
